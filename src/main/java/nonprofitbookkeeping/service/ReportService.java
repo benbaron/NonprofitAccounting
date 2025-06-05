2bc87667c3b448e2293a11e595f733b5114199bd
benbaron <benbaron00@gmail.com>
2025-06-04 22:04:27 -0600

    cleaned out some non-useful stuff

--- src/main/java/nonprofitbookkeeping/service/ReportService.java
+++ src/main/java/nonprofitbookkeeping/service/ReportService.java
@@ -7,8 +7,6 @@ import nonprofitbookkeeping.model.budget.BudgetLine;
 import nonprofitbookkeeping.model.budget.Periodicity;
 import nonprofitbookkeeping.reports.ReportContext;
 import nonprofitbookkeeping.reports.ReportMetadata;
-import nonprofitbookkeeping.reports.writer.LedgerReportWriter;
-
 import org.jxls.common.Context;
 import org.jxls.util.JxlsHelper;
 
@@ -1447,10 +1445,6 @@ public class ReportService
 		return outputFile;
 	}
 	
-	public void registerWriter(String reportType, LedgerReportWriter writer)
-	{
-		/* ... */ }
-		
 	public List<ReportMetadata> listGeneratedReports()
 	{
 		return new ArrayList<>();
