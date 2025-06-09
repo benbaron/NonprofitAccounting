639a05eec884a12e788e41902808e65c5cac8ae5
benbaron <benbaron00@gmail.com>
2025-06-08 19:43:02 -0600

    Keep fixing the AccountingEntry.java

--- src/main/java/nonprofitbookkeeping/model/AccountingEntry.java
+++ src/main/java/nonprofitbookkeeping/model/AccountingEntry.java
@@ -33,6 +33,18 @@ public final class AccountingEntry implements Serializable
 	/**
 	 * 
 	 * Constructor AccountingEntry
+	 */
+	public AccountingEntry ()
+	{
+		this.amount = null;
+		this.accountSide = null;
+		this.accountNumber = "";
+	}
+	
+	
+	/**
+	 * 
+	 * Constructor AccountingEntry
 	 * @param amount
 	 * @param accountNumber
 	 * @param accountSide
