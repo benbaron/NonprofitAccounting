4170aeb91b1a53c09ecb6c7d5b442d1bdbbf3ee7
benbaron <benbaron00@gmail.com>
2025-06-09 18:41:10 -0600

    Accounting Entry port

--- src/main/java/nonprofitbookkeeping/model/AccountingEntry.java
+++ src/main/java/nonprofitbookkeeping/model/AccountingEntry.java
@@ -26,7 +26,11 @@ public final class AccountingEntry implements Serializable
 	@JsonProperty final private BigDecimal amount;
 	@JsonProperty final private AccountSide accountSide;
 	@JsonProperty final private String accountNumber;
-	@JsonProperty private AccountingTransaction transaction;
+
+	// Future versions can include this.
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	private AccountingTransaction transaction;
+	
 	// Indicates if the transaction was set
 	@JsonProperty private boolean freeze = false;
 	
@@ -63,6 +67,7 @@ public final class AccountingEntry implements Serializable
 	 * Gets the associated transaction.
 	 * @return Associated transaction, or null if no transaction is associated.
 	 */
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 	public AccountingTransaction getTransaction()
 	{
 		return this.transaction;
