d517558a5b6c42c2dcee46e809290f05bd4b56e6
Ben Baron <benbaron00@gmail.com>
2025-06-15 15:40:57 -0600

    Refine persistence test

---
+++ src/test/java/nonprofitbookkeeping/model/CompanyPersistenceTest.java
@@ -0,0 +1,40 @@
+package nonprofitbookkeeping.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyPersistenceTest {
+
+    @Test
+    void accountsPersistAndReloadWithParentLinks() throws Exception {
+        Company company = new Company();
+        ChartOfAccounts chart = company.getChartOfAccounts();
+        Account parent = new Account("100", "Parent", AccountSide.DEBIT);
+        Account child = new Account("101", "Child", AccountSide.DEBIT);
+        chart.addAccount(parent);
+        chart.addSubAccount(parent, child);
+
+        File tempFile = File.createTempFile("company", ".npbk");
+        tempFile.deleteOnExit();
+
+        CurrentCompany.forceCompanyLoad(company);
+        CurrentCompany.setCurrentFile(tempFile);
+        CurrentCompany.persist();
+
+        // Reset loaded company
+        CurrentCompany.forceCompanyLoad(null);
+
+        CurrentCompany.loadFromPersistent(tempFile);
+        Company loaded = CurrentCompany.getCompany();
+
+        Account loadedChild = loaded.getChartOfAccounts().getAccount("101");
+        Account loadedParent = loaded.getChartOfAccounts().getAccount("100");
+
+        assertNotNull(loadedChild);
+        assertNotNull(loadedParent);
+        assertSame(loadedParent, loadedChild.getParentAccount());
+    }
+}
