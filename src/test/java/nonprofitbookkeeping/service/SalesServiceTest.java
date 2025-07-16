56262c8457f1e7c3a862330e9228d65d9d4903bd
benbaron <benbaron00@gmail.com>
2025-07-15 20:43:34 -0600

    Revert "report template cleanup"
    This reverts commit 44ed238fed44d389d16f02142662700b146287fb.

--- src/test/java/nonprofitbookkeeping/service/SalesServiceTest.java
+++ src/test/java/nonprofitbookkeeping/service/SalesServiceTest.java
@@ -1,4 +1,3 @@
-
 package nonprofitbookkeeping.service;
 
 import nonprofitbookkeeping.model.SaleRecord;
@@ -13,25 +12,22 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SalesServiceTest
-{
-	@TempDir Path tempDir;
-	
-	@Test
-		void saveAndLoad() throws IOException
-	{
-		File dir = this.tempDir.toFile();
-		SalesService service = new SalesService();
-		service
-			.addSale(new SaleRecord("1", "2024-01-01", "Item", 2, BigDecimal.TEN, BigDecimal.ONE));
-		service.saveSales(dir);
-		
-		SalesService load = new SalesService();
-		load.loadSales(dir);
-		List<SaleRecord> sales = load.listSales();
-		assertEquals(1, sales.size());
-		assertEquals("1", sales.get(0).getId());
-	}
-	
+public class SalesServiceTest {
+    @TempDir
+    Path tempDir;
+
+    @Test
+    void saveAndLoad() throws IOException {
+        File dir = this.tempDir.toFile();
+        SalesService service = new SalesService();
+        service.addSale(new SaleRecord("1", "2024-01-01", "Item", 2, BigDecimal.TEN, BigDecimal.ONE));
+        service.saveSales(dir);
+
+        SalesService load = new SalesService();
+        load.loadSales(dir);
+        List<SaleRecord> sales = load.listSales();
+        assertEquals(1, sales.size());
+        assertEquals("1", sales.get(0).getId());
+    }
 }
 
