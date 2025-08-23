2f1d401ccb2298980ff2c7232fa19df6bfc1b136
Ben Baron <benbaron00@gmail.com>
2025-08-22 20:08:33 -0600

    Provide default constructors for JPA entities

---
+++ src/main/java/nonprofitbookkeeping/model/RowBean.java
@@ -0,0 +1,10 @@
+package nonprofitbookkeeping.model;
+
+/**
+ * Placeholder for future auxiliary record support. Row beans will allow
+ * additional metadata to be attached to individual transactions. The
+ * implementation will include persistence mappings and UI integration.
+ */
+public class RowBean {
+        // TODO: define fields and persistence mapping for auxiliary records
+}
