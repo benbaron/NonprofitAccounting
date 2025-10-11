1fac33fc768f38fd20ab50a2f951727d9d00a16a
benbaron <benbaron00@gmail.com>
2025-10-11 17:28:33 -0600

    Another shot at h2

--- src/main/java/nonprofitbookkeeping/tools/JsonCompanyImporter.java
+++ src/main/java/nonprofitbookkeeping/tools/JsonCompanyImporter.java
@@ -11,7 +11,6 @@ import nonprofitbookkeeping.persistence.JournalRepository;
 import java.io.*;
 import java.nio.file.*;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.*;
 
