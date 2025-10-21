aee4bc24fc483f33bcefc22b157d77dcf301f312
Ben Baron <benbaron00@gmail.com>
2025-10-20 22:43:30 -0600

    Implement setters for Jasper datasource beans

--- src/main/java/nonprofitbookkeeping/reports/datasource/scareports/ExpenseDtl12aBean.java
+++ src/main/java/nonprofitbookkeeping/reports/datasource/scareports/ExpenseDtl12aBean.java
@@ -142,11 +142,9 @@ public class ExpenseDtl12aBean
 	/**
 	 * @param one
 	 */
-	public void setAmount(BigDecimal one)
-	{
-		// TODO Auto-generated method stub
-		
-	}
+        public void setAmount(BigDecimal amount)
+        {
+                this.amount = amount;
+        }
 
-	
 }
