fcec644372e7d7bd615845acdb941eaf3048d475
Ben Baron <benbaron00@gmail.com>
2025-10-21 08:50:28 -0600

    Fix SCA Jasper generators to use available beans

--- src/main/java/nonprofitbookkeeping/reports/jasper/TransferOut10JasperGenerator.java
+++ src/main/java/nonprofitbookkeeping/reports/jasper/TransferOut10JasperGenerator.java
@@ -1,7 +1,6 @@
 
 package nonprofitbookkeeping.reports.jasper;
 
-import java.math.BigDecimal;
 import java.util.*;
 import nonprofitbookkeeping.reports.datasource.scareports.TransferOut10Bean;
 
@@ -35,11 +34,14 @@ public class TransferOut10JasperGenerator extends AbstractReportGenerator
 	@Override
 	protected List<TransferOut10Bean> getReportData()
 	{
-		TransferOut10Bean bean = new TransferOut10Bean();
-		bean.setAmount(BigDecimal.ONE);
-		return java.util.Collections.singletonList(bean);
-		
-	}
+                TransferOut10Bean bean = new TransferOut10Bean();
+                bean.setTRANSFER_OUT_10_F25(Double.valueOf(1));
+                bean.setTRANSFER_OUT_10_F39(Double.valueOf(1));
+                bean.setTRANSFER_OUT_10_F51(Double.valueOf(1));
+                bean.setTRANSFER_OUT_10_F52(Double.valueOf(1));
+                return java.util.Collections.singletonList(bean);
+
+        }
 	
 	
 }
