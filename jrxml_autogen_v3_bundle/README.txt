6bc70f5d8b1ecf1d716680d2d5e2d951be0fec94
benbaron <benbaron00@gmail.com>
2025-11-04 07:36:02 -0700

    eliminate duplicate jrxmls

--- jrxml_autogen_v3_bundle/README.txt
+++
@@ -1,1 +0,0 @@
-JRXML v3: Independent X/Y scaling to fill page, styles before parameters, no style parent attribute, explicit font sizes.
--- jrxml_autogen_v3_bundle/beans/org/scaledger/reports/AccountBeans/FreeformBean.java
+++
@@ -1,7 +0,0 @@
-package org.scaledger.reports.AccountBeans;
-
-import java.util.Date;
-
-/** Auto-generated bean for sheet 'FreeForm'. */
-public class FreeformBean {
-}
