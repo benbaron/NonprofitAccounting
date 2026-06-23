caa41a5726cddc7d20f5ba9270e636f5ae849e01
Ben Baron <benbaron00@gmail.com>
2026-06-23 11:24:28 -0600

    Wire shared dashboard navigation for new shell

--- src/main/java/org/nonprofitbookkeeping/ui/AlternateDashboardPanel.java
+++ src/main/java/org/nonprofitbookkeeping/ui/AlternateDashboardPanel.java
@@ -1,6 +1,9 @@
 package org.nonprofitbookkeeping.ui;
 
+import javafx.application.Platform;
 import javafx.scene.Node;
+import javafx.stage.Window;
+import nonprofitbookkeeping.ui.LedgerNavigationContext;
 import nonprofitbookkeeping.ui.panels.DashboardNavigation;
 import nonprofitbookkeeping.ui.panels.SharedDashboardPanelFX;
 
@@ -15,13 +18,14 @@ public class AlternateDashboardPanel implements AppPanel
     public AlternateDashboardPanel(UiSessionContext sessionContext,
         UiServiceProvider services)
     {
-        this(sessionContext, services, new DashboardNavigation() { });
+        this(sessionContext, services, null);
     }
 
     public AlternateDashboardPanel(UiSessionContext sessionContext,
         UiServiceProvider services, DashboardNavigation navigation)
     {
-        this.dashboard = new SharedDashboardPanelFX(navigation);
+        this.dashboard = new SharedDashboardPanelFX(
+            navigation == null ? defaultNavigation() : navigation);
     }
 
     @Override
@@ -39,5 +43,84 @@ public class AlternateDashboardPanel implements AppPanel
     void refresh()
     {
         this.dashboard.reloadData();
+    }
+
+    private DashboardNavigation defaultNavigation()
+    {
+        return new DashboardNavigation()
+        {
+            @Override
+            public void openCashAndBank()
+            {
+                open(AppPanelId.LEDGER_REGISTER);
+            }
+
+            @Override
+            public void openLedger()
+            {
+                open(AppPanelId.LEDGER_REGISTER);
+            }
+
+            @Override
+            public void openFunds()
+            {
+                open(AppPanelId.FUNDS);
+            }
+
+            @Override
+            public void openReports()
+            {
+                open(AppPanelId.REPORTS_WORKSPACE);
+            }
+
+            @Override
+            public void openReconciliation()
+            {
+                open(AppPanelId.RECONCILIATION);
+            }
+
+            @Override
+            public void openUndepositedFunds()
+            {
+                Platform.runLater(() -> {
+                    MainWindowAlternate shell = findShell();
+                    if (shell != null)
+                    {
+                        shell.openUndepositedFundsDirect();
+                    }
+                });
+            }
+
+            @Override
+            public void openTransactionInLedger(int transactionId)
+            {
+                LedgerNavigationContext.requestTransaction(transactionId);
+                open(AppPanelId.LEDGER_REGISTER);
+            }
+
+            private void open(AppPanelId panelId)
+            {
+                Platform.runLater(() -> {
+                    MainWindowAlternate shell = findShell();
+                    if (shell != null)
+                    {
+                        shell.openPanel(panelId);
+                    }
+                });
+            }
+        };
+    }
+
+    private MainWindowAlternate findShell()
+    {
+        for (Window window : Window.getWindows())
+        {
+            if (window.isShowing() && window.getScene() != null &&
+                window.getScene().getRoot() instanceof MainWindowAlternate shell)
+            {
+                return shell;
+            }
+        }
+        return null;
     }
 }
